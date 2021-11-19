cd2af6a357df0328798e52c1412b5b5c9f7794e9
aditiramadwar <aditiramadwar@hotmail.com>
2021-11-18 22:18:49 -0500

    Applied algorithm for obstacle detection and avoidance

--- src/move.cpp
+++ src/move.cpp
@@ -15,14 +15,28 @@ class Move {
 };
 
 void Move::callback(const sensor_msgs::LaserScan::ConstPtr& msg) {
-  ROS_INFO("Laser scan heard: %f", msg->ranges[0]);
-  velocity.angular.z = 0.1;
+  float distance = 1000;
+  for (int i = 0; i < 60; i++) {
+    if (msg->ranges[i] < distance)
+      distance = msg->ranges[i];
+  }
+  ROS_INFO("Laser scan heard: %f", distance);
+  if (distance < 0.4) {
+    ROS_INFO("Very close!");
+    velocity.linear.x = 0.0;
+    velocity.angular.z = 1.0;
+  }
+  else {
+    ROS_INFO("Safe");
+    velocity.linear.x = 0.1;
+    velocity.angular.z = 0.0;
+  }
   pub.publish(velocity);
 }
 
 Move::Move(ros::NodeHandle& n) {
   laserscan = n.subscribe("/scan", 1000, &Move::callback, this);
-  pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+  pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
   ros::Rate loop_rate(2);
   while (n.ok()) {
     ros::spinOnce();
